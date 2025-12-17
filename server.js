6d9ba696e56c91a0e46316a47bbe28e0fe7a571a
Kanimozhi-E <kanimozhi.e2024it@sece.ac.in>
2025-12-17 15:34:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,4 +13,4 @@ app.use(express.json());
 app.use(cors());
 app.use("/api/students",studentRoutes);
 const PORT=process.env.PORT||5000;
-app.listen(PORT,()=>console.log(`server running on port ${PORT}`));
+app.listen(PORT,"0.0.0.0",()=>console.log(`server running on port ${PORT}`));
